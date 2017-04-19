2df23b38bd212cbcb53c2d5f618feb32ca29e675
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-04-19 16:58:10 -0700

    created a quick merchant new method

---
+++ app/controllers/merchants_controller.rb
@@ -0,0 +1,24 @@
+class MerchantsController < ApplicationController
+
+  def new
+    @merchant = Merchant.new
+  end
+
+  def create
+    @merchant = Merchant.new(merchant_params)
+    @merchant.save
+      # flash[:success] = "User added successfully"
+      # session[:user_id] = @user.id
+      # session[:username] = @user.name
+      # flash[:success] = "Successfully logged in as user #{@user.name} "
+    #   redirect_to root_path
+    # else
+    #   # flash.now[:failure] = "User did not save, try again"
+    #   # render :new, status: :bad_request
+    # end
+  end
+  private
+def merchant_params
+  params.require(:merchant).permit(:username, :email_address)
+end
+end
