8dac6c80f1758348a549778c3b589d48a366c870
Janice Lichtman <jalichtman@gmail.com>
2017-04-19 12:02:31 -0700

    added 2 more tests for validating status on Order model

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,13 +3,26 @@ require "test_helper"
 describe Order do
 
   describe "validations" do
-    it "requires a status" do
+    it "does not create an order without a status" do
       order = Order.new
       order.valid?.must_equal false
       order.errors.messages.must_include :status
     end
 
+    it "does not create an order with a status other than pending or paid" do
+      order = Order.new(status: "open")
+      order.valid?.must_equal false
+      order.errors.messages.must_include :status
+    end
 
+    it "successfully creates an order with a status of pending or paid" do
+      order1 = Order.new(status: "pending")
+      order2 = Order.new(status: "paid")
+
+      # This must go through, so we use save!
+      order1.save!
+      order2.save!
+    end
 
   end
 
