19e95e6a11ac0c1e86dd6e10c6468f1ab5bf237f
Sofia Kim <sofiamhkim@gmail.com>
2017-04-19 12:29:32 -0700

    added email validation under merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,5 @@
 class Merchant < ApplicationRecord
   has_many :products
   validates :username, presence: true, uniqueness: true
-
   validates :email, :presence => true, format: /.+@.+/
 end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -3,19 +3,20 @@ require "test_helper"
 describe Merchant do
   describe 'validations' do
     it "Can be created with name" do
-      a = Merchant.create!(username: "Dan")
+      a = Merchant.create!(username: "Dan", email: "222@adaworld.com")
       result = a.valid?
       result.must_equal true
     end
 
     it "Can be created with email" do
-
-
+      a = Merchant.create!(username: "Jamie", email: "111@adaworld.com")
+      result = a.valid?
+      result.must_equal true
     end
 
     it "email has '@' sign" do
-
-
+      a = Merchant.create!(username: "Sofi", email: "333@adaworld.com")
+      a.email.include? "@"
     end
   end
 end
