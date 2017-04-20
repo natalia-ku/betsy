336c736db5827a83962fef1b6677446ff368be9f
Sofia Kim <sofiamhkim@gmail.com>
2017-04-19 17:11:26 -0700

    added model tests for review

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -4,9 +4,49 @@ describe Review do
   describe 'validations' do
     it "Can be created with rating and review_text" do
       review = reviews(:one)
+      result = review.valid?
+      result.must_equal true
     end
 
-    it "ranges between 1-5"
-    
+    it "can't be created without rating" do
+      review = Review.new(review_text: "don't buy again")
+      result = review.valid?
+      result.must_equal false
+    end
+
+    it "can't be created without review_text" do
+      review = Review.new(rating: 4)
+      result = review.valid?
+      result.must_equal false
+    end
+
+    it "rating has to be between 1-5" do
+      review = Review.new(rating: 7, review_text: "don't buy again")
+      result = review.valid?
+      result.must_equal false
+    end
+
+    it "rating has to be between 1-5" do
+      review = Review.new(rating: -2, review_text: "don't buy again")
+      result = review.valid?
+      result.must_equal false
+    end
   end
+  describe 'relationship' do
+    it "I can access reviews for products" do
+      product_review = reviews(:one).product
+      product_review.must_equal products(:one)
+    end
+
+    it "has access product's price through review" do
+      product_review = reviews(:one).product.price
+      product_review.must_equal 20
+    end
+
+    it "has access product's stock through review" do
+      product_review = reviews(:one).product.stock
+      product_review.must_equal 2
+    end
+  end
+
 end
