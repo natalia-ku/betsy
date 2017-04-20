87bba6ce5ae9a3b28f75c94dfb7da9044e73c64b
Sofia Kim <sofiamhkim@gmail.com>
2017-04-19 20:59:18 -0700

    added tests under merchant model

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -2,10 +2,21 @@ require "test_helper"
 
 describe Merchant do
   describe 'validations' do
-    it "Can be created with name" do
+    it "requires a username" do
       a = Merchant.create!(username: "Dan", email: "222@adaworld.com")
       result = a.valid?
       result.must_equal true
+    end
+
+    it "requires a unique username" do
+      username = "test username"
+      merchant1 = Merchant.new(username: "username", email: "444@adaworld.com")
+      merchant1.save!
+
+      merchant2 = Merchant.new(username: "username", email: "555@adaworld.com")
+      result = merchant2.save
+      result.must_equal false
+      merchant2.errors.messages.must_include :username
     end
 
     it "Can be created with email" do
@@ -18,5 +29,15 @@ describe Merchant do
       a = Merchant.create!(username: "Sofi", email: "333@adaworld.com")
       a.email.include? "@"
     end
+
+    # describe "relations" do
+    #   it "has a list of products"
+    #
+    #   dan = merchants(:dan)
+    #   dan.must_be_respond_to :products
+    #   dan.products.each do |product|
+    #   product.must_be_kind_of Product
+    #
+    #  end
   end
 end
