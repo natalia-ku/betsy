a94972b384b5a828d1f748b400c99d8aacb4a03a
Janice Lichtman <jalichtman@gmail.com>
2017-04-21 16:14:05 -0700

    created index route, action, and view, and 2 passing tests for it

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+    describe "index" do
+      it "succeeds with many products" do
+        # Assumption: there are many users in the DB
+
+        Product.count.must_be :>, 0
+        get products_path
+        must_respond_with :success
+      end
+
+      it "succeeds with no products" do
+        # Start with a clean slate
+        Product.destroy_all
+        get products_path
+        must_respond_with :success
+      end
+    end
 end
