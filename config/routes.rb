ecefe193c8b72d4329a0ee8e121bb1eee0c6e292
Janice Lichtman <jalichtman@gmail.com>
2017-04-21 15:42:29 -0700

    realized i'd misspelled controller name. Destroyed and recreated products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,25 @@
 Rails.application.routes.draw do
-  root to: 'products#index'
+  #root to: 'products#index'
+  get  'products', to:  'products#index'
+
+  # resources :products
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :merchants, only: [:show, :index]
+  # resources :merchants, only: [:show, :index]
+  #
+  # get 'login', to: 'merchants#new'
+  #
+  # post 'login', to: "merchants#create"
+  #
+  # delete 'logout', to: "merchants#destroy"
 
-  get 'login', to: 'merchants#new'
 
-  post 'login', to: "merchants#create"
 
-  delete 'logout', to: "merchants#destroy"
-
-  resources :products
-  
   # resources :merchants do
   #    resources :products, only: [:show, :new, :create, :index]
   #  end
   #
   #  resources :products
-
 
 
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-describe ProdcutsController do
+describe ProductsController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
