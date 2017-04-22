2c8bf2dedd71ff337a83d2ab3e734c8ae826e6a3
natalia-ku <bilberryfm@mail.ru>
2017-04-21 18:31:49 -0700

    added edit,sho  to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,4 +12,27 @@ class OrdersController < ApplicationController
       redirect_to send(:back)
     end
   end
+
+  def shopping_cart
+    @order = current_order
+    @shopping_cart_products = OrderProduct.all.where(order_id: @order.id)
+    #@shopping_cart_products = OrderProduct.all.where(status???)
+  end
+
+  # def paid_order # when user clicks pay button in the checkout
+  #   @order.status = "paid"
+  # end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def edit
+    @order = current_order
+  end
+
+
+
+
+  
 end
