389ea90825d518616d1ab3e0cf091db4acdaccfc
natalia-ku <bilberryfm@mail.ru>
2017-04-21 18:33:01 -0700

    added destroy  to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,8 +31,38 @@ class OrdersController < ApplicationController
     @order = current_order
   end
 
+  def update #order completed
+    @order = current_order
 
+    # @order.update_attributes(order_params) # NOT WORKING!!!
+    @order.email = params[:email]
+    @order.mailing_address = params[:mailing_address]
+    @order.card_name = params[:card_name]
+    @order.credit_card = params[:credit_card]
+    @order.cvv = params[:cvv]
+    @order.zip_code = params[:zip_code]
+    @order.status = "completed"
+    @order.paid_at = @order.updated_at
+    if @order.save!
+      flash[:success] = "You successfully created your order"
+      redirect_to order_path(@order.id)
+      session[:order_id] = nil
+    else
+      render :edit
+    end
+  end
 
+  def destroy
+    current_order.order_products.each do |op|
+      op.destroy
+    end
+    current_order.destroy
+    session[:order_id] = nil
+    redirect_to products_path
+  end
 
-  
+  private
+  def order_params
+    params.require(:order_params).permit(:status, :email, :mailing_address,:card_name, :credit_card, :cvv, :zip_code, :paid_at)
+  end
 end
