8a7d4ee6fb355d52d58c118f20e79c4a0e62dff6
natalia-ku <bilberryfm@mail.ru>
2017-04-21 18:26:57 -0700

    added merchant controller actions

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,24 +1,31 @@
 class MerchantsController < ApplicationController
 
+  def index
+    @merchants = Merchant.all
+  end
+
+  def show
+    @merchant = Merchant.find_by(id: params[:id])
+    if @merchant.nil?
+      head :not_found
+    end
+  end
+
+
   def new
     @merchant = Merchant.new
   end
 
+
   def create
     @merchant = Merchant.new(merchant_params)
-    @merchant.save
-      # flash[:success] = "User added successfully"
-      # session[:user_id] = @user.id
-      # session[:username] = @user.name
-      # flash[:success] = "Successfully logged in as user #{@user.name} "
-    #   redirect_to root_path
-    # else
-    #   # flash.now[:failure] = "User did not save, try again"
-    #   # render :new, status: :bad_request
-    # end
+    if @merchant.save
+      redirect_to merchants_path
+    end
+
   end
   private
-def merchant_params
-  params.require(:merchant).permit(:username, :email)
-end
+  def merchant_params
+    params.require(:merchant).permit(:username, :email)
+  end
 end
