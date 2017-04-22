4251c76bb0195cd02ac6465f1cb7f05ffa9e110f
natalia-ku <bilberryfm@mail.ru>
2017-04-21 18:28:35 -0700

    added nested routes for product and erchant

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :merchants
+  resources :merchants do
+    resources :products, only: [:show, :new, :create, :index]
+  end
+
+  resources :products
+
+  resources :orders
+  resources :order_products
+
+  get "shopping_cart", to: 'orders#shopping_cart'
+
+  put "checkout", to: 'orders#checkout'
 end
