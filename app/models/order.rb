34a309fa5bf549ee56d09caf7d79d4529bf16e9e
natalia-ku <bilberryfm@mail.ru>
2017-04-21 18:35:53 -0700

    added methods to models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ApplicationRecord
   has_many :order_products
   has_many :products, through: :order_products
+
+  def total_price
+    op = OrderProduct.where(order_id: self.id)
+    total = 0.0
+    op.each do |product|
+      total += product.subtotal
+    end
+    return total
+  end
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -3,4 +3,7 @@ class OrderProduct < ApplicationRecord
   belongs_to :product
 
   validates :quantity, presence: true,  numericality:{:greater_than => 0 }
+  def subtotal
+    return self.quantity * self.product.price
+  end
 end
