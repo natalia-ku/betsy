cbb8523ff0ca7e3a05e470574dd30884238d3399
natalia-ku <bilberryfm@mail.ru>
2017-04-21 18:37:41 -0700

    added strict params and destroy  actions to orderproducts controller

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -12,4 +12,22 @@ class OrderProductsController < ApplicationController
     @order_product.save
     redirect_to shopping_cart_path
   end
+
+
+
+
+
+
+  def destroy
+  @order = current_order
+  @order_product = OrderProduct.find(params[:id])
+  @order_product.product.stock += @order_product.quantity #when deleting product from cart need to update product stock back
+  @order_product.destroy
+  redirect_to shopping_cart_path
+end
+
+private
+def order_products_params
+  params.require(:order_products_params).permit(:quantity, :order_id, :product_id)
+end
 end
