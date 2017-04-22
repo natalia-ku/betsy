efeb77d0d05cbe756384d863d6c0b5517aad3a4a
natalia-ku <bilberryfm@mail.ru>
2017-04-21 18:27:40 -0700

    added products controller actions- new and create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+  def index
+  @products = Product.all
+end
+def new
+  @product = Product.new
+  @merchant = Merchant.find(params[:merchant_id])
+end
+
+def create
+  @merchant = Merchant.find(params[:merchant_id])
+  @product = @merchant.products.create(product_params)
+end
 end
