5b24ffa072380682f71b72ff127dfa2c9c15ed6e
natalia-ku <bilberryfm@mail.ru>
2017-04-21 18:37:14 -0700

    added new and create actions to orderproducts controller

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -1,3 +1,15 @@
 class OrderProductsController < ApplicationController
-  
+  def new
+    @order_product = OrderProduct.new
+  end
+
+  def create
+    @order_product = OrderProduct.new(quantity: params[:quantity], order_id: params[:order_id], product_id: params[:product_id])
+    @order_product.product.stock = @order_product.product.stock - params[:quantity].to_i
+    @order_product.quantity = params[:quantity].to_i
+
+    @order_product.product.save
+    @order_product.save
+    redirect_to shopping_cart_path
+  end
 end
