c7cedcd33020d00d3cba31f81c525233f8afda49
natalia-ku <bilberryfm@mail.ru>
2017-04-21 18:31:06 -0700

    added index,new create to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,15 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+  def new
+    @order = Order.new(order_params)
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to send(:back)
+    end
+  end
 end
