2cc6c98940e3a9f2e2166c2b17918f05e9b1758d
natalia-ku <bilberryfm@mail.ru>
2017-04-21 18:29:29 -0700

    added show and destroy methods to product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,36 @@
 class ProductsController < ApplicationController
   def index
-  @products = Product.all
-end
-def new
-  @product = Product.new
-  @merchant = Merchant.find(params[:merchant_id])
-end
+    @products = Product.all
+  end
+  def new
+    @product = Product.new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
 
-def create
-  @merchant = Merchant.find(params[:merchant_id])
-  @product = @merchant.products.create(product_params)
-end
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    @product = @merchant.products.create(product_params)
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    if @product.nil?
+      head :not_found
+    end
+    @order_product = OrderProduct.new(product: @product, order: current_order)
+    # ??? do we need current order here???
+    @current_order = current_order
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+
+      redirect_to products_path
+    end
+  end
+  private
+  def product_params
+    return params.require(:product).permit( :name, :photo_url, :price, :stock )
+  end
 end
