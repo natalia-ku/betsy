1cb66633ef22cc85f73480a785f984ebfc0d207d
Janice Lichtman <jalichtman@gmail.com>
2017-04-22 17:43:52 -0700

    wrote and passed tests for products index action when called with a given merchant_id

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,26 +3,43 @@ require "test_helper"
 
 describe ProductsController do
   describe "index" do
-    it "succeeds with many products" do
-      # Assumption: there are many users in the DB
+    it "succeeds with many products, when not called with a particular merchant_id" do
+      # Assumption: There are many products in the DB
 
       Product.count.must_be :>, 0
       get products_path
       must_respond_with :success
     end
 
-    it "succeeds with no products" do
+    it "succeeds with no products, when not called with a particular merchant_id" do
       # Start with a clean slate
       Product.destroy_all
       get products_path
       must_respond_with :success
     end
-  end
 
-  describe "new" do
-    it "runs successfully" do
-      get new_product_path
+    it "succeeds with many products, when  called with a particular merchant_id" do
+      # Assumption: That merchant has products
+      dan = merchants(:dan)
+      dan.products.count.must_be :>, 0
+      get merchant_products_path(dan)
       must_respond_with :success
     end
+
+    it "succeeds with no products, whencalled with a particular merchant_id" do
+    # Assumption: That merchant has no products
+      Product.destroy_all
+      dan = merchants(:dan)
+      get merchant_products_path(dan)
+      must_respond_with :success
+    end
+
   end
+
+  # describe "new" do
+  #   it "runs successfully" do
+  #     get new_product_path
+  #     must_respond_with :success
+  #   end
+  # end
 end
