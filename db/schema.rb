67782392f92817bcbc27700cbb68bc0d3826e01a
Janice Lichtman <jalichtman@gmail.com>
2017-04-24 14:10:47 -0700

    added column in products table for retired boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422203327) do
+ActiveRecord::Schema.define(version: 20170424210545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,8 +69,9 @@ ActiveRecord::Schema.define(version: 20170422203327) do
     t.text     "description"
     t.integer  "stock"
     t.integer  "merchant_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "retired",     default: false
     t.index ["merchant_id"], name: "index_products_on_merchant_id", using: :btree
   end
 
