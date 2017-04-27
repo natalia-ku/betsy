803ce941ca052f9f69813dbb3a4c16195e4dfbe5
Janice Lichtman <jalichtman@gmail.com>
2017-04-27 10:00:49 -0700

    updated ship function to only allow shipping when order status is paid

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #root to: 'merchants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :merchants, only: [:show, :index]
+  resources :merchants, only: [:show, :index]  ``
 
   delete 'logout', to: "merchants#logout"
 
