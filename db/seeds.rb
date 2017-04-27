e2e7c9380c699a62a9212aa9af21f4cda17f2ccd
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-04-27 12:43:26 -0700

    simplecov?

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 Category.destroy_all
 Product.destroy_all
@@ -32,9 +25,9 @@ end
   product.description = Faker::Lorem.paragraph
   product.stock = rand(1..10)
   product.merchant_id = Merchant.all.sample.id
-
+  product.categories = Category.all.sample
   product.save!
-  product_category = ProductCategory.create({:category_id => Category.all.sample.id, :product_id => product.id})
+  # product_category = ProductCategory.create({:category_id => Category.all.sample.id, :product_id => product.id})
 end
 
 
